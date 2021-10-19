21609accc6422c255bbb0bb0bee161a055bf3530
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2021-10-19 15:53:38 -0400

    add loading for movies

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-   const fetchMoviesHandler = async () => {
+  const fetchMoviesHandler = async () => {
+    setIsLoading(true);
     const response = await fetch('https://swapi.dev/api/films');
     const data = await response.json();
       
@@ -18,6 +20,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   };
 
   return (
@@ -26,7 +29,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MovieList movies={movies} />
+        {!isLoading && movies.length > 0 && <MovieList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
